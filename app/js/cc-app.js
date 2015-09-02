25fad1a71b4d64cb5c481da18774d78f0a6a774c
Eunice Kim <ekimed@gmail.com>
2015-09-02 11:49:24 -0700

    gulp tasks

---
+++ app/js/cc-app.js
@@ -0,0 +1,7 @@
+angular.module('ccApp', ['ngRoute', 'ngAnimate'])
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: './views/home.html',
+			controller: 'HomeCtrl'
+		});
+	});
