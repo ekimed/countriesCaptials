b563f2816d38f6b21dfef0baffdda82476870ae6
Eunice Kim <ekimed@gmail.com>
2015-09-09 17:05:47 -0700

    fixed some styling

--- app/js/cc-app.js
+++ app/js/cc-app.js
@@ -1,15 +1,25 @@
-angular.module('ccApp', ['ngRoute', 'ngAnimate'])
-	.config(['$routeProvider', function ($routeProvider) {
-		$routeProvider.when('/', {
-			templateUrl: 'views/home.html',
-			controller: 'HomeCtrl'
-		})
-		.when('/countries', {
-			templateUrl: 'views/country.html',
-			controller: "CountryCtrl"
-		})
-		.when('/capitals', {
-			templateUrl: 'views/ctryDetail.html',
-			controller: "CtryDetailCtrl"
-		});
+(function () {
+	angular.module('ccApp', ['ngRoute', 'ngAnimate'])
+	.config(['$routeProvider', routeConfig]);
 	}]);
+
+	function routeConfig ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/home.html',
+				controller: 'HomeCtrl'
+			})
+			.when('/countries', {
+				templateUrl: 'views/country.html',
+				controller: 'CountryCtrl'
+			})
+			.when('/countries/:country/captial', {
+				templateUrl: 'views/detail.html',
+				controller: 'DetailCtrl'
+			})
+			.otherwise({
+				redirectTo: '/countries'
+			});
+	}
+	
+}());
